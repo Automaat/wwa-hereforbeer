7a47cc8b8787687d693ca462f866d3541aa228ed
Automaat <marcin0skalski@gmail.com>
2017-03-24 21:40:36 +0100

    initial commit - project setup

---
+++ src/main/java/com/hereforbeer/AuctionHelperApplication.java
@@ -0,0 +1,12 @@
+package com.hereforbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionHelperApplication.class, args);
+	}
+}
