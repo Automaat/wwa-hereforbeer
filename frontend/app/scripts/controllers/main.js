ea8974bcf932a4e0d2d52f7e5140ad78d9011486
laganows <g.laganowski94@gmail.com>
2017-03-25 01:50:19 +0100

    HFB: Frontend  Initial commit

---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name auctionHelperApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the auctionHelperApp
+ */
+angular.module('auctionHelperApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
