2cbeb71a6cb7e73cca47e77c6bc9a13c880c1437
laganows <g.laganowski94@gmail.com>
2017-03-25 12:01:45 +0100

    HFB: Frontend Other changes for charts

--- frontend/app/scripts/controllers/top_offers_gaps.js
+++ frontend/app/scripts/controllers/top_offers_gaps.js
@@ -5,6 +5,101 @@
 angular.module('auctionHelperApp')
   .controller('TopOffersGapsCtrl', function (serverAddress, $scope, $http) {
 
+    $scope.today = function() {
+      $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function() {
+      $scope.dt = null;
+    };
+
+    $scope.inlineOptions = {
+      customClass: getDayClass,
+      minDate: new Date(),
+      showWeeks: true
+    };
+
+    $scope.dateOptions = {
+      dateDisabled: disabled,
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22),
+      minDate: new Date(),
+      startingDay: 1
+    };
+
+    // Disable weekend selection
+    function disabled(data) {
+      var date = data.date,
+      mode = data.mode;
+      var today = new Date();
+      return mode === 'day' && false;
+    }
+
+    $scope.toggleMin = function() {
+      $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+      $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+    };
+
+    $scope.toggleMin();
+
+    $scope.open1 = function() {
+      $scope.popup1.opened = true;
+    };
+
+    $scope.open2 = function() {
+      $scope.popup2.opened = true;
+    };
+
+    $scope.setDate = function(year, month, day) {
+      $scope.dt = new Date(year, month, day);
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+    $scope.altInputFormats = ['M!/d!/yyyy'];
+
+    $scope.popup1 = {
+      opened: false
+    };
+
+    $scope.popup2 = {
+      opened: false
+    };
+
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date();
+    afterTomorrow.setDate(tomorrow.getDate() + 1);
+    $scope.events = [
+      {
+        date: tomorrow,
+        status: 'full'
+      },
+      {
+        date: afterTomorrow,
+        status: 'partially'
+      }
+    ];
+
+    function getDayClass(data) {
+      var date = data.date,
+        mode = data.mode;
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+        for (var i = 0; i < $scope.events.length; i++) {
+          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
+          }
+        }
+      }
+
+      return '';
+    }
+
     $scope.offers = [];
     $scope.selectedItem = "pie";
 
@@ -40,7 +135,7 @@ angular.module('auctionHelperApp')
               type: 'pie'
             },
             title: {
-              text: 'Browser market shares January, 2015 to May, 2015'
+              text: 'Browser market shares from ' + $scope.dt + ' to ' + new Date()
             },
             tooltip: {
               pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -77,7 +172,7 @@ angular.module('auctionHelperApp')
               type: 'column'
             },
             title: {
-              text: 'World\'s largest cities per 2014'
+              text: 'Browser market shares from ' + $scope.dt + ' to ' + new Date()
             },
             subtitle: {
               text: ''
